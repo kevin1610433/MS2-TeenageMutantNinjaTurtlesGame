7af9a204c53dc717cf724c27c607d2fc3f078dda
kevin1610433 <slay1610@gmail.com>
2020-10-28 20:30:52 +0000

    making some changes to layout

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,7 +5,7 @@ $('#myModal').on('shown.bs.modal', function () {
 //code found on https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ and credit goes to them//
 const cardsArray = [
   {
-    name: 'ninja1',
+    name: 'ninja',
     img: 'assets/images/ninja1.jpg',
   },
 
@@ -34,6 +34,17 @@ const cardsArray = [
     img: 'assets/images/ninja1.jpg',
   },
 
-]
+];
+
+//Code taken from Karen Yuen fellow Student
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0; 
+let previousTarget = null;
+let delay = 1200;
+let timerOn = false;
+let matchCount = 0;
+
 
   
