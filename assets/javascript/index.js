a2d795ec38378559855e673ec5cc833078a97c32
kevin1610433 <slay1610@gmail.com>
2020-10-28 17:50:00 +0000

    added in some Js from code i have found and accredited, added timer in html and changed some styling

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,39 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+//code found on https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ and credit goes to them//
+const cardsArray = [
+  {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+   {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+   {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+   {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+   {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+   {
+    name: 'ninja1',
+    img: 'assets/images/ninja1.jpg',
+  },
+
+]
+
+  
