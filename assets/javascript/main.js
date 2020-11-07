2e41d8df808dc6e61111b2bcdbe0b4604a8d949e
kevin1610433 <slay1610@gmail.com>
2020-11-07 16:41:09 +0000

    adding cards and styling them with CSS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,52 +1,7 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+const cards = document.querySelectorAll('.memory-card');
 
-//code found on https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/ and credit goes to them//
-const cardsArray = [
-  {
-    name: 'ninja',
-    img: 'assets/images/ninja1.jpg',
-  },
+function flipCard() {
+  this.classList.toggle('flip');
+}
 
-   {
-    name: 'ninja1',
-    img: 'assets/images/ninja1.jpg',
-  },
-
-   {
-    name: 'ninja1',
-    img: 'assets/images/ninja1.jpg',
-  },
-
-   {
-    name: 'ninja1',
-    img: 'assets/images/ninja1.jpg',
-  },
-
-   {
-    name: 'ninja1',
-    img: 'assets/images/ninja1.jpg',
-  },
-
-   {
-    name: 'ninja1',
-    img: 'assets/images/ninja1.jpg',
-  },
-
-];
-
-//Code taken from Karen Yuen fellow Student
-
-let firstGuess = '';
-let secondGuess = '';
-let count = 0; 
-let previousTarget = null;
-let delay = 1200;
-let timerOn = false;
-let matchCount = 0;
-
-let game = document.getElementById('game');
-let grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
+cards.forEach(card => card.addEventListener('click', flipCard));
