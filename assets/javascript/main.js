b965ef56ae7be25d140af1d6b1ced74e85ce3107
kevin1610433 <slay1610@gmail.com>
2020-11-08 15:36:17 +0000

    adding some JS for flipping the cards around

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,49 @@
 const cards = document.querySelectorAll('.memory-card');
 
  let hasFlippedCard = false;
+ let lockBoard = false;
  let firstCard, secondCard;
 
   function flipCard() {
+      if (lockBoard) return;
    this.classList.add('flip');
 
    if (!hasFlippedCard) {
      hasFlippedCard = true;
      firstCard = this;
+        return;
    }
-  }
+
+   secondCard = this;
+   hasFlippedCard = false;
+
+   checkForMatch();
+ }
+
+ function checkForMatch() {
+   if (firstCard.dataset.framework === secondCard.dataset.framework) {
+     disableCards();
+     return;
+   }
+
+   unflipCards();
+ }
+
+ function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+ }
+
+ function unflipCards() {
+     lockBoard = true;
+   setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+   
+   lockBoard = false;
+    }, 
+   
+   1500);
+ }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
