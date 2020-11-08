ec67d0568e75cdfe1920258e3de67cbd011bf881
kevin1610433 <slay1610@gmail.com>
2020-11-08 15:58:27 +0000

    adding some more JS to help with the flipping of the cards

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,14 @@
 const cards = document.querySelectorAll('.memory-card');
 
+
+
  let hasFlippedCard = false;
  let lockBoard = false;
  let firstCard, secondCard;
 
   function flipCard() {
       if (lockBoard) return;
+      if (this === firstCard) return;
    this.classList.add('flip');
 
    if (!hasFlippedCard) {
@@ -15,7 +18,6 @@ const cards = document.querySelectorAll('.memory-card');
    }
 
    secondCard = this;
-   hasFlippedCard = false;
 
    checkForMatch();
  }
@@ -32,6 +34,8 @@ const cards = document.querySelectorAll('.memory-card');
  function disableCards() {
    firstCard.removeEventListener('click', flipCard);
    secondCard.removeEventListener('click', flipCard);
+
+   resetBoard();
  }
 
  function unflipCards() {
@@ -40,10 +44,16 @@ const cards = document.querySelectorAll('.memory-card');
      firstCard.classList.remove('flip');
      secondCard.classList.remove('flip');
    
-   lockBoard = false;
+
+   resetBoard();
     }, 
    
    1500);
+ }
+
+  function resetBoard() {
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
  }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
