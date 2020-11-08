0f1bb67ad15d11b0e5302db984cea8edf52d9b2f
kevin1610433 <slay1610@gmail.com>
2020-11-08 16:03:43 +0000

    adding shuffle function for the card game

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -56,4 +56,11 @@ const cards = document.querySelectorAll('.memory-card');
    [firstCard, secondCard] = [null, null];
  }
 
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 12);
+     card.style.order = ramdomPos;
+   });
+ })();
+
 cards.forEach(card => card.addEventListener('click', flipCard));
