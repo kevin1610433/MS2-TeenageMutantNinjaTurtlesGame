da189251cec5cb905a2841e920252df7a43ae406
kevin1610433 <slay1610@gmail.com>
2020-11-14 12:58:11 +0000

    adding in timer button for game

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,4 @@
-//Code taken from and accredited to https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
-
-
-const cards = document.querySelectorAll(".memory-card");
-
+// Code taken from W3Schools
 
 var music = document.getElementById("myAudio");
 function playAudio() {
@@ -13,6 +9,46 @@ var music = document.getElementById("myAudio");
 function pauseAudio() {
 music.pause();
 }
+
+var music = document.getElementById("myAudio");
+function restartAudio() {
+music.restart();
+}
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+ 
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementsById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "GAME OVER";
+  }
+}, 1000);
+
+//Code taken from and accredited to https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
+
+const cards = document.querySelectorAll(".memory-card");
+
+
+
 
 let hasFlippedCard = false;
 let lockBoard = false;
